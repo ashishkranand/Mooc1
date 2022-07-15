29b7c04607f720c83e324d4e488e7c64077bc010
Ashish kumar anand <ashishraj5296@gmail.com>
2022-07-15 14:45:03 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function hindi(){
+    var element = document.getElementById("hindi").style.display="block";
+    var element = document.getElementById("english").style.display="none";
+    var element = document.getElementById("maithili").style.display="none";
+    var element = document.getElementById("novel").style.display="none";
+}
+function english(){
+    var element = document.getElementById("english").style.display="block";
+    var element = document.getElementById("hindi").style.display="none";
+    var element = document.getElementById("maithili").style.display="none";
+    var element = document.getElementById("novel").style.display="none";
+}
+function maithili(){
+    var element = document.getElementById("maithili").style.display="block";
+    var element = document.getElementById("hindi").style.display="none";
+    var element = document.getElementById("english").style.display="none";
+    var element = document.getElementById("novel").style.display="none";
+
+}
+function novel(){
+    var element = document.getElementById("novel").style.display="block";
+    var element = document.getElementById("maithili").style.display="none";
+    var element = document.getElementById("hindi").style.display="none";
+    var element = document.getElementById("english").style.display="none";
+}
